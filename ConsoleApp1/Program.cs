7b5ca723d5182f8fb71a273c9bb01c4d74bde1a0
octosup <4eap8er@gmail.com>
2023-02-20 14:27:31 +0300

    first app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine($"Тип данных: byte __ MIN: {byte.MinValue} __ MAX:: {byte.MaxValue} __ Размер: {sizeof(byte)}" +
+    $"\n \nТип данных: int __ MIN: {int.MinValue} __ MAX:: {int.MaxValue} __ Размер: {sizeof(int)}" +
+    $"\n \n Тип данных: decimal __ MIN: {decimal.MinValue} __ MAX:: {decimal.MaxValue} __ Размер: {sizeof(decimal)}" +
+    $"\n \nТип данных: sbyte __ MIN: {sbyte.MinValue} __ MAX:: {sbyte.MaxValue} __ Размер: {sizeof(sbyte)}" +
+    $"\n \nТип данных: uint __ MIN: {uint.MinValue} __ MAX:: {uint.MaxValue} __ Размер: {sizeof(uint)}" +
+    $"\n \nТип данных: short __ MIN: {short.MinValue} __ MAX:: {short.MaxValue} __ Размер: {sizeof(short)}" +
+    $"\n \nТип данных: double __ MIN: {double.MinValue} __ MAX:: {double.MaxValue} __ Размер: {sizeof(double)}" +
+    $"\n \nТип данных: ushort __ MIN: {ushort.MinValue} __ MAX:: {ushort.MaxValue} __ Размер: {sizeof(ushort)}" +
+    $"\n \nТип данных: float __ MIN: {float.MinValue} __ MAX:: {float.MaxValue} __ Размер: {sizeof(float)}" +
+    $"\n \nТип данных: long __ MIN: {long.MinValue} __ MAX:: {long.MaxValue} __ Размер: {sizeof(long)}" +
+    $"\n \nТип данных: long __ MIN: {ulong.MinValue} __ MAX:: {ulong.MaxValue} __ Размер: {sizeof(ulong)}" +
+    $"\n \nТип данных: bool __ Возможные логические значения: true __ false" +
+    $"\n \nТип данных: char __ Возможные диапазоны: От U+0000 до U+FFFF __ Функция: Используется для хранения целочисленного значения члена представительной кодировки" +
+    $"\n \nТип данных: object __ Функция: Считается базовым классом для всех остальных классов и типов, включая и типы значений.");
